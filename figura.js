9fba875953fbfff6522d43686b4f00e133ca4bcd
michaelo <michaelbohorquez2003@gmail.com>
2022-06-30 15:13:09 -0500

    first commit

---
+++ figura.js
@@ -0,0 +1,6 @@
+const ladocuadrado=5;
+console.log("los lados del cuadrado miden: " + ladocuadrado + "cm");
+const perimetrocuadradro = ladocuadrado*4;
+console.log("El perimetro del cuadrado es: " + perimetrocuadradro+ "cm") ;
+const areacuadradro = ladocuadrado*ladocuadrado;
+console.log("El área del cuadrado es: " + areacuadradro + "cm");
